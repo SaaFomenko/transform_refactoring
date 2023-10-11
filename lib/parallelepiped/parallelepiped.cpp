21d111ef71283c162e004754ca2123a51f4f0268
SaaFomenko <s.fomenko@list.ru>
2023-10-11 19:23:35 +0300

    Save to start class Parallelepiped.

---
+++ lib/parallelepiped/parallelepiped.cpp
@@ -0,0 +1,5 @@
+#include "../shape/shape.h"
+#include "../line/line.h"
+
+
+
---
+++ lib/parallelepiped/parallelepiped.h
@@ -0,0 +1,12 @@
+#ifndef PARALLELEPIPED_H
+#define PARALLELEPIPED_H
+
+#include "../shape/shape.h"
+
+
+class Parallelepiped : public Shape
+{
+	
+}
+
+#endif
